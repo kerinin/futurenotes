c48bbd557b5c9641be59897954561aefe86c8496
Ryan Michael <kerinin@gmail.com>
2011-10-23 14:50:49 -0500

    adding devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Futurenotes::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Required by devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
